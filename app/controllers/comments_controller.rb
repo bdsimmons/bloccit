6ef931e73b615ca97d0e157144a226af8a80067b
Ben <ben.simmons@trinityis.com>
2014-04-22 21:08:28 -0500

    Destroyed topics, posts and comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:post_id])
+    @post = @topic.posts.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
     Rails.logger.info ">>>>>>>>>>>>>>>> #{@comment.inspect}"
@@ -17,6 +17,22 @@ class CommentsController < ApplicationController
     else
       flash[:error] = "There was an error saving your comment. Please try again."
       render :new
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+
+    @comment = @post.comments.find(params[:id])
+
+    authorize @comment
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
+      redirect_to [@topic, @post]
     end
   end
 
