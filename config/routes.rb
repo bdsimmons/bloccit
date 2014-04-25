81838378f2040103f65ed9d92acb09dc23419fea
Ben <ben.simmons@trinityis.com>
2014-04-25 02:02:38 -0500

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Bloccit::Application.routes.draw do
   
+  get "posts/index"
   devise_for :users
 
+  resources :posts, only: [:index]
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       resources :favorites, only: [:create, :destroy]
       get '/up-vote' => 'votes#up_vote', as: :up_vote
@@ -11,7 +13,7 @@ Bloccit::Application.routes.draw do
     end
   end
 
-  resources :users, only: [:show, :update]
+  resources :users, only: [:show, :index, :update]
 
   get 'about' => "welcome#about"
 
