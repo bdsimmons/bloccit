9f3b3544a7b18312856693e6dedb7cd762d02a59
Ben <ben.simmons@trinityis.com>
2014-04-24 22:08:49 -0500

    Interlude changes for registration, email permissions and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,10 @@ class User < ActiveRecord::Base
     self.favorites.where(post_id: post.id).first
   end
 
+  def voted(post)
+    self.votes.where(post_id: post.id).first
+  end
+
   private
   
 end
